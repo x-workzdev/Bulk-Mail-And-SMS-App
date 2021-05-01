60e5889d08b9b09b92cd461b7dbf2934f6e5d319
= <=>
2021-05-01 19:08:12 +0530

    updated bulk mail and sms app

---
+++ src/main/java/com/xworkz/service/SMSService.java
@@ -0,0 +1,10 @@
+package com.xworkz.service;
+
+public interface SMSService {
+	public String sendSMS(String apiKey, String username, String sender, String phone, String message,
+			String smsType,String route,String templateId);
+	
+	public String deliveryReports(String apiKey, String username, String apiRequest ,String messageId);
+	
+	public String checkSMSBalance(String apiKey, String username, String apiRequest ,String route);
+}
